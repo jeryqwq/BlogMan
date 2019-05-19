f2216387474c18dab1440b28ad8c73e0558271fc
CJ <jery1997@foxmail.com>
2019-05-19 23:19:37 +0800

    “compile docs file”

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: 'BlogMan',
     description: '曾今我也以为梦想遥不可及',
+    base:'https://github.com/jeryqwq/BlogMan',
     themeConfig: {
         nav: [
             { text: '前端', link: '/' },
